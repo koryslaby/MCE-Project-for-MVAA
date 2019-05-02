7ae75062a9795d4a947e34ed7629dfc4e8270197
David Lewis <lewisde@gmail.com>
2019-05-02 14:14:13 -0400

    cleaned up csv files, used data.py to add data to sqlite3 database. will continue work on adding courses and outcomes.

--- files/data.py
+++ files/data.py
@@ -1,9 +1,37 @@
-data = []
-with open("outcomedata.csv") as f:
-	lines = f.readlines()
-	for line in lines:
-		indata = line.strip().split(':')
-		print(indata)
-		data.append(indata)
+import sqlite3
 
+con = sqlite3.connect('mce.sqlite3')
+cur = con.cursor()
 
+#data = []
+#with open("outcomedata.csv") as f:
+#	lines = f.readlines()
+#	for line in lines:
+#		indata = line.strip().split(':')
+#		print(indata[0])
+
+with open("institution.csv") as f:
+    lines = f.readlines()
+    for line in lines[1:]:
+        inst1 = line.strip().split('::')
+        sql = "insert into Institution (InstitutionName, InstitutionAddress, InstitutionCity, InstitutionState, InstitutionZipcode, InstitutionWebsite) values (?, ?, ?, ?, ?, ?);"
+        cur.execute(sql, (inst1[0], inst1[1], inst1[2], inst1[3], inst1[4], inst1[5]))		
+        
+with open("course.csv") as f:
+    lines = f.readlines()
+    for line in lines[1:]:
+        inst2 = line.strip().split('::')
+        print(inst2)
+        sql = "insert into Course (CourseNumber, CourseName, CourseDescription, CourseCredit, CourseEquivalenceNonOC, InstitutionID) values (?, ?, ?, ?, ?, ?);"
+        cur.execute(sql, (inst2[0], inst2[1], inst2[2], inst2[3], inst2[4], inst2[5]))
+
+with open("outcome.csv") as f:
+    lines = f.readlines()
+    for line in lines[1:]:
+        inst3 = line.strip().split('::')
+        sql = "insert into Outcome (OutcomeDescription, CourseNumber) values (?, ?);"
+        cur.execute(sql, (inst3[1], inst3[0]))
+
+con.commit()
+
+con.close()
