bffbf57eb6ef79d226bf2fbb6d795a8334da7126
David Lewis <lewisde@gmail.com>
2019-05-14 14:00:59 -0400

    split outcomes for NV-2201-0128 and A-830-0030

--- files/clean.py
+++ files/clean.py
@@ -1,8 +1,8 @@
-with open('JSToutcomes.csv', 'r') as f:
+with open('outcome.csv', 'r') as f:
     lines = f.readlines()
     s = set()
     for line in lines:
-        s.add(line.strip().split("::")[0])
-    with open('JST.csv', 'w') as g:
+        s.add(line)
+    with open('clean.csv', 'w') as g:
         for item in sorted(s):
-            g.write(item + "\n")
+            g.write(item)
