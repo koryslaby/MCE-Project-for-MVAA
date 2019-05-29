6ab85c5d78e5c6191c6359dfc74ba5a1bb32d7e8
David Lewis <lewisde@gmail.com>
2019-05-29 14:21:42 -0400

    commit moved files

--- clean.py
+++
@@ -1,8 +0,0 @@
-with open('outcome.csv', 'r') as f:
-    lines = f.readlines()
-    s = set()
-    for line in lines:
-        s.add(line)
-    with open('clean.csv', 'w') as g:
-        for item in sorted(s):
-            g.write(item)
