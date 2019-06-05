d739c548dc98dbbd3dc11a1a699ec862159029e3
David Lewis <lewisde@gmail.com>
2019-06-05 11:56:50 -0400

    added Reviewer.py with constructor

---
+++ Reviewer.py
@@ -0,0 +1,7 @@
+class Reviewer():
+
+    def __init__(self, name, phone, email, dept):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.department = dept
